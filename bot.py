a374287f6b43738ab073f251982666b1af661a8a
Sengy <104589954+SengyEU@users.noreply.github.com>
2022-08-30 13:52:16 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import time
 
 client = commands.Bot(command_prefix='.',intents=discord.Intents.all())
 
@@ -16,11 +17,15 @@ async def on_message(message):
     messagenow = messages[0]
     messagebackstr = messageback.content[-1]
     messagenowstr = messagenow.content[0]
-
-    if(messagebackstr != messagenowstr):
-      await message.delete()
-    else:
-      return
     
+    if (messagenow.author.bot or messageback.author.bot):
+      return
+    else:
+      if(messagebackstr.lower() != messagenowstr.lower()):
+          await message.reply('První písmeno tvého slova se neshoduje s posledím písmenem slova předchozího.', delete_after=10)
+          time.sleep(5)
+          await message.delete()
+      else:
+        return  
 
 client.run('token')
